26a6315f9754b5504ca42dd7724908bea9cb7969
desc3nd <gogogo321.mc@gmail.com>
2021-04-03 17:06:17 +0200

    fixed memory leek using valgrind

---
+++ cmake-build-debug/CMakeFiles/nowy.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/BstTree.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/BstTree.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/DynamicArray.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/DynamicArray.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/List.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/List.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/TimeTests.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/TimeTests.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/bin_heap.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/bin_heap.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/main.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/main.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/stopWatch.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/stopWatch.cpp.o"
+  "/home/marcinek/Pulpit/tests-of-heap-array-and-list/structureTests.cpp" "/home/marcinek/Pulpit/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/structureTests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/nowy.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nowy.dir/BstTree.cpp.o"
+  "CMakeFiles/nowy.dir/DynamicArray.cpp.o"
+  "CMakeFiles/nowy.dir/List.cpp.o"
+  "CMakeFiles/nowy.dir/TimeTests.cpp.o"
+  "CMakeFiles/nowy.dir/bin_heap.cpp.o"
+  "CMakeFiles/nowy.dir/main.cpp.o"
+  "CMakeFiles/nowy.dir/stopWatch.cpp.o"
+  "CMakeFiles/nowy.dir/structureTests.cpp.o"
+  "nowy"
+  "nowy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
