158da7db4ed5c01d17f28f0a3c8c4915ee2cf1a9
desc3nd <gogogo321.mc@gmail.com>
2021-04-09 16:59:19 +0200

    eh

---
+++ cmake-build-debug/CMakeFiles/nowy.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/BstTree.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/BstTree.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/DataGenerator.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/DataGenerator.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/DynamicArray.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/DynamicArray.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/List.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/List.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/TimeTests.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/TimeTests.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/bin_heap.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/bin_heap.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/main.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/main.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/menu.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/menu.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/stopWatch.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/stopWatch.cpp.obj"
+  "C:/studia 2k19/projekty/tests-of-heap-array-and-list/structureTests.cpp" "C:/studia 2k19/projekty/tests-of-heap-array-and-list/cmake-build-debug/CMakeFiles/nowy.dir/structureTests.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/nowy.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nowy.dir/BstTree.cpp.obj"
+  "CMakeFiles/nowy.dir/DataGenerator.cpp.obj"
+  "CMakeFiles/nowy.dir/DynamicArray.cpp.obj"
+  "CMakeFiles/nowy.dir/List.cpp.obj"
+  "CMakeFiles/nowy.dir/TimeTests.cpp.obj"
+  "CMakeFiles/nowy.dir/bin_heap.cpp.obj"
+  "CMakeFiles/nowy.dir/main.cpp.obj"
+  "CMakeFiles/nowy.dir/menu.cpp.obj"
+  "CMakeFiles/nowy.dir/stopWatch.cpp.obj"
+  "CMakeFiles/nowy.dir/structureTests.cpp.obj"
+  "libnowy.dll.a"
+  "nowy.exe"
+  "nowy.exe.manifest"
+  "nowy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
